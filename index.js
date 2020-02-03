74567bfc6e28d4f7076e2630272c1b2c185d8505
jejdacz <markhunter@seznam.cz>
2020-02-03 20:23:19 +0100

    designed

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var messages = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.emit('history', JSON.stringify(messages));
+  socket.on('chat message', function(msg){    
+    const m = {text:msg,date:Date.now()}; 
+    messages.push(m);   
+    io.emit('chat message', JSON.stringify(m));  
   });
 });
 
