d48d0d4efaf348821a5f71e8dbfbb4e77ed6dca6
jejdacz <markhunter@seznam.cz>
2020-02-04 23:19:29 +0100

    parcel

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var messages = [];
+var messages = {};
+
+app.use(express.static('dist'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.emit('history', JSON.stringify(messages));
-  socket.on('chat message', function(msg){    
-    const m = {text:msg,date:Date.now()}; 
-    messages.push(m);   
-    io.emit('chat message', JSON.stringify(m));  
+  socket.emit('connection', JSON.stringify(messages));
+  socket.on('message', function(msg){    
+    const m = {text:msg,date:Date.now(),done:false}; 
+    messages = {...messages,[m.date]:m};   
+    io.emit('message', JSON.stringify(m));
+    console.log(messages);  
+  });
+  socket.on('done',function(msg){
+    messages[msg].done = true;
+    io.emit('done', msg);
+  });
+  socket.on('undone',function(msg){
+    messages[msg].done = false;
+    io.emit('undone', msg);
   });
 });
 
---
+++ src/main.js
@@ -0,0 +1,82 @@
+import "./styles.scss";
+import io from "./socket.io";
+
+let messages = {};
+
+const scrollDown = () => document.querySelector("#messages").scrollTo(0, document.querySelector("#messages").scrollHeight);
+
+const toggleDone = (id) => {
+  messages[id].done  ? socket.emit("undone", id) : socket.emit("done", id);  
+};
+
+const createMessage = msg => {
+  const date = new Date(msg.date);
+
+  const $d = document.createElement("small");
+  $d.innerText =
+    date
+      .getHours()
+      .toString()
+      .padStart(2, "0") +
+    " : " +
+    date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+  const $t = document.createElement("p");
+  $t.innerText = msg.text;
+  const $m = document.createElement("li");
+  $m.id = "id"+msg.date;
+  $m.append($d);
+  $m.append($t);  
+  $m.classList.add("rounded");
+  
+  $m.onclick = function() {    
+    toggleDone(msg.date);
+  };
+
+  if (msg.done) {
+    $m.classList.add("done");
+  }
+  
+  return $m;
+};
+
+var socket = io();
+
+document.querySelector("form").onsubmit = function() {
+  socket.emit("message", document.querySelector("#m").value);
+  document.querySelector("#m").value = "";
+  return false;
+};
+
+socket.on("message", function(msg) {
+  const message = JSON.parse(msg);
+  document.querySelector("#messages").append(createMessage(message));
+  messages[message.date] = message;
+
+  scrollDown();
+});
+
+socket.on("done", function(id) {
+  messages[id].done = true;
+  document.querySelector("#id"+id).classList.add("done");
+});
+
+socket.on("undone", function(id) {
+  messages[id].done = false;
+  document.querySelector("#id"+id).classList.remove("done");
+});
+
+socket.on("connection", function(msg) {
+  const serverHistory = JSON.parse(msg);
+  
+  messages = {...serverHistory};
+  
+  for (const m in messages) {
+    document.querySelector("#messages").append(createMessage(messages[m]));
+  }
+  
+  scrollDown();
+});
+
