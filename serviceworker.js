cc2313639673e553ed95bd33cb70da237bf53ab7
jejdacz <markhunter@seznam.cz>
2020-02-04 13:52:15 +0100

    flexbox

--- serviceworker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('notificationclose', function(e) {
-    var notification = e.notification;
-    var primaryKey = notification.data.primaryKey;
-  
-    console.log('Closed notification: ' + primaryKey);
-  });
